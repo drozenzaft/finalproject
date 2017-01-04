a2a3d772d261a10722e35a0aa3cc8cbbe7b02490
Ida Wang <iwang2@stuy.edu>
2017-01-03 22:30:20 -0500

    basic flow layout

---
+++ Layout.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Layout extends JFrame implements ActionListener{
+
+    private Container pane;
+
+    private JButton calculate;
+    private JLabel start, end, route;
+    private JTextField sstation, estation;
+
+    public Layout(){
+	this.setTitle("IAD's Trip Planner");
+	this.setSize(1000,800);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+
+	calculate = new JButton("CALCULATE ROUTE");
+	calculate.addActionListener(this);
+	calculate.setActionCommand("calculate");
+	
+	start = new JLabel("START");
+	end = new JLabel("END");
+	route = new JLabel("DIRECTIONS: ");
+	
+	sstation = new JTextField(12);
+	estation = new JTextField(12);
+
+	pane.add(start);
+	pane.add(sstation);
+	pane.add(end);
+	pane.add(estation);
+	pane.add(calculate);
+	pane.add(route);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("calculate")){
+	    route.setText("DIRECTIONS: added directions");
+	}
+    }
+    
+    public static void main(String[]args){
+	Layout a = new Layout();
+	a.setVisible(true);
+    }
+}

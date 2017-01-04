0e9210fcaedd9d0511ad0fc01189bf1ae7a6ad17
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-01-03 23:47:45 -0500

    In writing the last commit message, I forgot that I am an idiot; it completely slipped my mind that we learned about Scanners. This is the basic scanner for our data file. We are undecided on what data to use, so I put a placeholder data.csv file that does not pertain to our project; its only purpose is to test the scanner in this file, and it will be deleted as soon as we decide on which data file we will be using.

--- CSVRoute.java
+++ CSVRoute.java
@@ -1,6 +1,41 @@
-public class CSVRoute {
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-	public static void main(String[] args) {
-	
-	}	
+public class CSVRoute {
+    private ArrayList<String> data;
+    public CSVRoute() {
+	data = loadData();
+    }
+    public static ArrayList<String> loadData() {
+	ArrayList<String> temp = new ArrayList<String>();
+	try {
+	    Scanner dataScan = new Scanner(new File("data.csv"));
+	    String line = "";
+	    int i = 0;
+	    while (dataScan.hasNext() && i < 20) {
+		line = dataScan.next();
+		temp.add(line);
+		i++;
+	    }
+	    dataScan.close();
+	}
+	catch (FileNotFoundException e) {
+	    System.out.println("File not found! Please have a valid 'data.csv' file!");
+	    System.exit(1);
+	}
+	return temp;
+    }
+    public void printData() {
+	for (String line : data) {
+	    System.out.println(line);
+	}
+    }
+    
+    public static void main(String[] args) {
+	CSVRoute csv = new CSVRoute();
+	csv.printData();
+    }
 }
